8aa3f987d0d00d73a779e564c40308646db0e2ad
Sui Chen <suichen@google.com>
2020-10-28 16:35:53 -0700

    Add empty IPMI blob handler
    This change adds an IPMI blob handler to phosphor-health-monitor.
    Currently, the IPMI blob handler does not do anything yet; however, the
    necessary dependencies are added such that the IPMI blob handler is
    successfully built.
    
    The most important dependency is the addition of "phosphor-ipmi-blobs"
    since this library provides the necessary header files that define the
    interfaces the blob handler .so libraries talk to IPMID. However,
    phosphor-ipmi-blobs currently does not include a pkgconfig so it cannot
    be found by Meson. To enable meson to discover it, the following change
    needs to be applied first:
    https://gerrit.openbmc-project.xyz/c/openbmc/phosphor-ipmi-blobs/+/37133
    The CI is expected to fail until the change 37133 is merged.
    
    Signed-off-by: Sui Chen <suichen@google.com>
    Depends-On: I126c66dbd65ddbd99f921297d813e7a0f9981f22
    Change-Id: I92311f8165c567d6be86b7ecd91a9396d895e938

---
+++ ipmi/main.cpp
@@ -0,0 +1,26 @@
+#include <blobs-ipmid/blobs.hpp>
+#include <phosphor-logging/elog.hpp>
+
+#include <memory>
+
+using namespace phosphor::logging;
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    /**
+     * This is required by the blob manager.
+     * TODO: move the declaration to blobs.hpp since all handlers need it
+     */
+    std::unique_ptr<blobs::GenericBlobInterface> createHandler();
+
+#ifdef __cplusplus
+}
+#endif
+
+std::unique_ptr<blobs::GenericBlobInterface> createHandler()
+{
+    return nullptr;
+}
