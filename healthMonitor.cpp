fa2d91f42ac489be1da9c6bf00a32251b63bdfd6
Sui Chen <suichen@google.com>
2021-11-01 15:28:51 -0700

    Add kernel and user CPU utilization
    This adds two utilization sensors, "CPU_Kernel" and "CPU_User" that
    are populated with the fraction of CPU processing spent in kernel space
    and user space.
    
    The intended use case is BMCWeb can read those sensors from
    phosphor-health-monitor and report them in a RedFish response.
    
    The RedFish resources are:
    * ManagerDiagnosticData.ProcessorStatistics.KernelPercent
    * ManagerDiagnosticData.ProcessorStatistics.UserPercent
    
    Tested: Installed on a QEMU-emulated BMC
    bmc# busctl tree xyz.openbmc_project.HealthMon
    `-/xyz
      `-/xyz/openbmc_project
        `-/xyz/openbmc_project/sensors
          `-/xyz/openbmc_project/sensors/utilization
            |-/xyz/openbmc_project/sensors/utilization/CPU
            |-/xyz/openbmc_project/sensors/utilization/CPU_Kernel
            `-/xyz/openbmc_project/sensors/utilization/CPU_User
    
    signed-off-by: Sui Chen <suichen@google.com>
    Change-Id: Ic6792e4c7cd8eba144eb9adec9366c1bc15b1a44

--- healthMonitor.cpp
+++ healthMonitor.cpp
@@ -103,7 +103,14 @@ enum CPUStatesTime
     NUM_CPU_STATES_TIME
 };
 
-double readCPUUtilization([[maybe_unused]] std::string path)
+enum CPUUtilizationType
+{
+    USER = 0,
+    KERNEL,
+    TOTAL
+};
+
+double readCPUUtilization(enum CPUUtilizationType type)
 {
     auto proc_stat = "/proc/stat";
     std::ifstream fileStat(proc_stat);
@@ -142,22 +149,33 @@ double readCPUUtilization([[maybe_unused]] std::string path)
         return -1;
     }
 
-    static double preActiveTime = 0, preIdleTime = 0;
+    static std::unordered_map<enum CPUUtilizationType, double> preActiveTime, preIdleTime;
     double activeTime, activeTimeDiff, idleTime, idleTimeDiff, totalTime,
         activePercValue;
 
     idleTime = timeData[IDLE_IDX] + timeData[IOWAIT_IDX];
-    activeTime = timeData[USER_IDX] + timeData[NICE_IDX] +
-                 timeData[SYSTEM_IDX] + timeData[IRQ_IDX] +
-                 timeData[SOFTIRQ_IDX] + timeData[STEAL_IDX] +
-                 timeData[GUEST_USER_IDX] + timeData[GUEST_NICE_IDX];
+    if (type == TOTAL)
+    {
+        activeTime = timeData[USER_IDX] + timeData[NICE_IDX] +
+                     timeData[SYSTEM_IDX] + timeData[IRQ_IDX] +
+                     timeData[SOFTIRQ_IDX] + timeData[STEAL_IDX] +
+                     timeData[GUEST_USER_IDX] + timeData[GUEST_NICE_IDX];
+    }
+    else if (type == KERNEL)
+    {
+        activeTime = timeData[SYSTEM_IDX];
+    }
+    else if (type == USER)
+    {
+        activeTime = timeData[USER_IDX];
+    }
 
-    idleTimeDiff = idleTime - preIdleTime;
-    activeTimeDiff = activeTime - preActiveTime;
+    idleTimeDiff = idleTime - preIdleTime[type];
+    activeTimeDiff = activeTime - preActiveTime[type];
 
     /* Store current idle and active time for next calculation */
-    preIdleTime = idleTime;
-    preActiveTime = activeTime;
+    preIdleTime[type] = idleTime;
+    preActiveTime[type] = activeTime;
 
     totalTime = idleTimeDiff + activeTimeDiff;
 
@@ -167,6 +185,21 @@ double readCPUUtilization([[maybe_unused]] std::string path)
         std::cout << "CPU Utilization is " << activePercValue << "\n";
 
     return activePercValue;
+}
+
+auto readCPUUtilizationTotal([[maybe_unused]] const std::string& path)
+{
+    return readCPUUtilization(CPUUtilizationType::TOTAL);
+}
+
+auto readCPUUtilizationKernel([[maybe_unused]] const std::string& path)
+{
+    return readCPUUtilization(CPUUtilizationType::KERNEL);
+}
+
+auto readCPUUtilizationUser([[maybe_unused]] const std::string& path)
+{
+    return readCPUUtilization(CPUUtilizationType::USER);
 }
 
 double readMemoryUtilization([[maybe_unused]] const std::string& path)
@@ -285,7 +318,9 @@ constexpr auto storage = "Storage";
 constexpr auto inode = "Inode";
 /** Map of read function for each health sensors supported */
 const std::map<std::string, std::function<double(const std::string& path)>>
-    readSensors = {{"CPU", readCPUUtilization},
+    readSensors = {{"CPU", readCPUUtilizationTotal},
+                   {"CPU_User", readCPUUtilizationUser},
+                   {"CPU_Kernel", readCPUUtilizationKernel},
                    {"Memory", readMemoryUtilization},
                    {storage, readStorageUtilization},
                    {inode, readInodeUtilization}};
@@ -582,7 +617,6 @@ std::vector<HealthConfig> HealthMon::getHealthConfig()
 {
 
     std::vector<HealthConfig> cfgs;
-    HealthConfig cfg;
     auto data = parseConfigFile(HEALTH_CONFIG_FILE);
 
     // print values
