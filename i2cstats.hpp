d4ade7ff42889253184d6efc3959fb168b512e17
Sui Chen <suichen@google.com>
2021-12-09 13:59:45 -0800

    [WIP] Add I2C statistics
    This is a WIP.
    
    The I2C statistics are intended to be used with Redfish resources:
    
    ManagerDiagnosticData.I2CBuses.I2CBusName
    ManagerDiagnosticData.I2CBuses.TotalTransactionCount
    ManagerDiagnosticData.I2CBuses.BusErrorCount
    ManagerDiagnosticData.I2CBuses.NACKCount
    
    Notes:
    
    1) This change will not build unless the I2CStats interfaces have been
    added to phosphor-dbus-interfaces and the change
    https://gerrit.openbmc-project.xyz/c/openbmc/phosphor-dbus-interfaces/+/45683
    is merged. This change currently serves the purpose for helping
    discussing how the I2C statistics should be maintained as they add new
    code paths to phosphor-health-monitor.
    
    2) Currently, the I2C statistics are only available on NPCM7XX I2C
    controllers, because only this device exports those statistics. This
    device exports the statistics to debugfs. We have reached out to the
    LKML about adding mechanisms to allow I2C controller drivers to export
    counters to sysfs, allowing a unified interface across different I2C
    controllers, but we haven't received a concrete answer from them yet.
    
    Change-Id: Ib29221d0b2b4f8bb60748ff0f4bf110a68b8419d
    Signed-off-by: Sui Chen <suichen@google.com>

---
+++ i2cstats.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdeventplus/clock.hpp>
+#include <sdeventplus/event.hpp>
+#include <sdeventplus/utility/timer.hpp>
+#include <xyz/openbmc_project/I2C/I2CStats/server.hpp>
+
+#include <memory>
+
+using I2CStatsInterface =
+    sdbusplus::xyz::openbmc_project::I2C::server::I2CStats;
+using I2CStatsObjectInterface =
+    sdbusplus::server::object::object<I2CStatsInterface>;
+
+// Nuvoton I2C DebugFS statistics example:
+// SysFS Path:    /sys/class/i2c-dev/i2c-X
+// DebugFS Path:  /sys/kernel/debug/npcm_i2c/f0080000.i2c
+// Files:         ber_cnt, nack_cnt, rec_fail_cnt, rec_succ_cnt, timeout_cnt
+
+// Only physical I2C buses are included.
+
+class I2CStatsDBusObject : I2CStatsObjectInterface
+{
+  public:
+    I2CStatsDBusObject(const std::string& objectPath,
+                       const std::string& debugfsPath, const int i2cBusId,
+                       sdbusplus::bus::bus& bus);
+    void readI2CStat();
+
+  private:
+    sdbusplus::bus::bus& bus;
+    std::string debugfsPath;
+    int i2cBusId;
+};
+
+class I2CStats
+{
+  public:
+    I2CStats(sdbusplus::bus::bus& bus);
+    void initializeI2CStatsDBusObjects();
+    void startReadLoop();
+
+  private:
+    sdbusplus::bus::bus& bus;
+    std::vector<std::shared_ptr<I2CStatsDBusObject>> i2cStatsObjects;
+    sdeventplus::Event timerEvent;
+    sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic> readTimer;
+    void readI2CStats();
+};
