ecef75680ac46a7c351a56bd536a12951a540348
Jagpal Singh Gill <paligill@gmail.com>
2023-12-08 11:41:41 -0800

    phosphor-health-monitor: add health_metric_config
    Add the health_metric_config interface and implementation for
    phosphor-health-monitor. This interface will be used in the
    re-write of phosphor-health-monitor.
    This change is in relation to following design and D-Bus interface update -
    https://gerrit.openbmc.org/c/openbmc/docs/+/64917
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/64914
    
    gtest added for UT.
    
    Change-Id: Ic40faafbb57597023cc70036428d46ee69a895a2
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ test/test_health_metric_config.cpp
@@ -0,0 +1,55 @@
+#include "health_metric_config.hpp"
+
+#include <sdbusplus/test/sdbus_mock.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace phosphor::health::metric::config;
+
+constexpr auto minConfigSize = 1;
+
+TEST(HealthMonitorConfigTest, TestConfigSize)
+{
+    auto healthMetricConfigs = getHealthMetricConfigs();
+    EXPECT_GE(healthMetricConfigs.size(), minConfigSize);
+}
+
+bool isValidSubtype(MetricType type, MetricSubtype subType)
+{
+    switch (type)
+    {
+        case MetricType::CPU:
+            return (subType == MetricSubtype::CPUTotal ||
+                    subType == MetricSubtype::CPUTotal ||
+                    subType == MetricSubtype::CPUUser);
+        case MetricType::memory:
+            return (subType == MetricSubtype::memoryAvailable ||
+                    subType == MetricSubtype::memoryBufferedAndCached ||
+                    subType == MetricSubtype::memoryFree ||
+                    subType == MetricSubtype::memoryShared ||
+                    subType == MetricSubtype::memoryTotal);
+        case MetricType::storage:
+        case MetricType::inode:
+            return (subType == MetricSubtype::NA);
+        default:
+            return false;
+    }
+}
+
+TEST(HealthMonitorConfigTest, TestConfigValues)
+{
+    auto healthMetricConfigs = getHealthMetricConfigs();
+    for (const auto& [type, configs] : healthMetricConfigs)
+    {
+        EXPECT_NE(type, MetricType::unknown);
+        EXPECT_GE(configs.size(), minConfigSize);
+        for (const auto& config : configs)
+        {
+            EXPECT_NE(config.metricName, std::string(""));
+            EXPECT_TRUE(isValidSubtype(type, config.metricSubtype));
+            EXPECT_GE(config.collectionFrequency, defaultFrequency);
+            EXPECT_GE(config.windowSize, defaultWindowSize);
+            EXPECT_GE(config.thresholdConfigs.size(), minConfigSize);
+        }
+    }
+}
