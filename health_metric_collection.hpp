a66ec1f503948af03c20cdaa9065c7dac4f51d84
Jagpal Singh Gill <paligill@gmail.com>
2023-12-10 15:33:05 -0800

    phosphor-health-monitor: add the health_metric_collection interface
    Add the interface for health_metric_collection which encapsulates
    various health_metrics.
    This change is in relation to following design and D-Bus interface update -
    https://gerrit.openbmc.org/c/openbmc/docs/+/64917
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/64914
    
    Change-Id: Ia0b9fbc6bec4850735c7eb74dcd5c40fc47c568c
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ health_metric_collection.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "health_metric.hpp"
+
+namespace phosphor
+{
+namespace health
+{
+namespace metric
+{
+namespace collection
+{
+using namespace phosphor::health::metric::config;
+using namespace phosphor::health::metric;
+
+class HealthMetricCollection
+{
+  public:
+    virtual ~HealthMetricCollection() = default;
+    HealthMetricCollection(
+        sdbusplus::bus_t& bus, MetricType metricType,
+        const std::vector<HealthMetricConfig>& healthMetricConfigs,
+        const std::vector<std::string>& bmcInventoryPaths) :
+        bus(bus),
+        metricType(metricType), healthMetricConfigs(healthMetricConfigs)
+    {
+        createHealthMetricCollection(bmcInventoryPaths);
+    }
+
+    void readHealthMetricCollection();
+
+  private:
+    /** @brief D-Bus bus connection */
+    sdbusplus::bus_t& bus;
+    void createHealthMetricCollection(
+        const std::vector<std::string>& bmcInventoryPaths);
+    MetricType metricType;
+    const std::vector<HealthMetricConfig>& healthMetricConfigs;
+    std::unordered_map<MetricSubtype, std::unique_ptr<HealthMetric>>
+        healthMetrics;
+    std::unordered_map<MetricSubtype, double> healthMetricCache;
+};
+
+} // namespace collection
+} // namespace metric
+} // namespace health
+} // namespace phosphor
