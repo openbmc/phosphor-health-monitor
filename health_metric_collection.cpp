e1355ec0bb6a6ee5f2503a968bb18d283ffa589f
Jagpal Singh Gill <paligill@gmail.com>
2023-12-10 15:33:05 -0800

    phosphor-health-monitor: add the health_metric_collection
    Add the interface and implementation for health_metric_collection which
    encapsulates various health_metrics.
    This change is in relation to following design and D-Bus interface update -
    https://gerrit.openbmc.org/c/openbmc/docs/+/64917
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/64914
    
    Add test_health_metric_collection gtest for unit testing.
    
    Change-Id: Ia0b9fbc6bec4850735c7eb74dcd5c40fc47c568c
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ health_metric_collection.cpp
@@ -0,0 +1,272 @@
+#include "health_metric_collection.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+
+#include <fstream>
+#include <numeric>
+#include <unordered_map>
+
+PHOSPHOR_LOG2_USING;
+
+namespace phosphor::health::metric::collection
+{
+
+constexpr auto procStat = "/proc/stat";
+constexpr auto procMeminfo = "/proc/meminfo";
+
+constexpr auto metricCPU = "cpu";
+constexpr auto memoryTotal = "MemTotal";
+constexpr auto memoryFree = "MemFree";
+constexpr auto memoryAvailable = "MemAvailable";
+constexpr auto memoryShared = "Shmem";
+constexpr auto memoryBuffers = "Buffers";
+constexpr auto memoryCached = "Cached";
+
+constexpr auto BMCInventoryItem = "xyz.openbmc_project.Inventory.Item.Bmc";
+
+constexpr auto BytesMultiplier = 1000;
+
+enum CPUStatsIndex
+{
+    userIndex = 0,
+    niceIndex,
+    systemIndex,
+    idleIndex,
+    iowaitIndex,
+    irqIndex,
+    softirqIndex,
+    stealIndex,
+    guestUserIndex,
+    guestNiceIndex,
+    maxCPUStatsIndex
+};
+
+static bool
+    readCPUMetric(const std::vector<HealthMetricConfig>& healthMetricConfigs,
+                  std::unordered_map<MetricSubtype, double>& healthMetricCache)
+{
+    std::ifstream fileStat(procStat);
+    if (!fileStat.is_open())
+    {
+        error("Unable to open {PATH} for reading CPU stats", "PATH", procStat);
+        return -1;
+    }
+
+    std::string firstLine, labelName;
+    std::size_t timeData[CPUStatsIndex::maxCPUStatsIndex];
+
+    std::getline(fileStat, firstLine);
+    std::stringstream ss(firstLine);
+    ss >> labelName;
+
+    debug("CPU Stats Label Name is {LABEL_NAME}", "LABEL_NAME", labelName);
+
+    if (labelName.compare(metricCPU))
+    {
+        error("CPU data not available");
+        return false;
+    }
+
+    for (auto idx = 0; idx < CPUStatsIndex::maxCPUStatsIndex; idx++)
+    {
+        if (!(ss >> timeData[idx]))
+        {
+            error("CPU data not correct");
+            return false;
+        }
+    }
+
+    static std::unordered_map<MetricSubtype, uint64_t> preActiveTime,
+        preTotalTime;
+
+    for (auto& metricConfig : healthMetricConfigs)
+    {
+        // These are actually Jiffies. On the BMC, 1 jiffy usually corresponds
+        // to 0.01 second.
+        uint64_t activeTime = 0, activeTimeDiff = 0, totalTime = 0,
+                 totalTimeDiff = 0;
+        double activePercValue = 0;
+
+        if (metricConfig.metricSubtype == MetricSubtype::CPUTotal)
+        {
+            activeTime = timeData[CPUStatsIndex::userIndex] +
+                         timeData[CPUStatsIndex::niceIndex] +
+                         timeData[CPUStatsIndex::systemIndex] +
+                         timeData[CPUStatsIndex::irqIndex] +
+                         timeData[CPUStatsIndex::softirqIndex] +
+                         timeData[CPUStatsIndex::stealIndex] +
+                         timeData[CPUStatsIndex::guestUserIndex] +
+                         timeData[CPUStatsIndex::guestNiceIndex];
+        }
+        else if (metricConfig.metricSubtype == MetricSubtype::CPUKernel)
+        {
+            activeTime = timeData[CPUStatsIndex::systemIndex];
+        }
+        else if (metricConfig.metricSubtype == MetricSubtype::CPUUser)
+        {
+            activeTime = timeData[CPUStatsIndex::userIndex];
+        }
+
+        totalTime = std::accumulate(std::begin(timeData), std::end(timeData),
+                                    0);
+
+        activeTimeDiff = activeTime - preActiveTime[metricConfig.metricSubtype];
+        totalTimeDiff = totalTime - preTotalTime[metricConfig.metricSubtype];
+
+        /* Store current idle and active time for next calculation */
+        preActiveTime[metricConfig.metricSubtype] = activeTime;
+        preTotalTime[metricConfig.metricSubtype] = totalTime;
+
+        activePercValue = (100.0 * activeTimeDiff) / totalTimeDiff;
+        debug("CPU Metric {METRIC_SUBTYPE}: {METRIC_VALUE}", "METRIC_SUBTYPE",
+              std::to_underlying(metricConfig.metricSubtype), "METRIC_VALUE",
+              (double)activePercValue);
+
+        healthMetricCache[metricConfig.metricSubtype] = activePercValue;
+    }
+
+    return true;
+}
+
+static bool readMemoryMetric(
+    const std::vector<HealthMetricConfig>& healthMetricConfigs,
+    std::unordered_map<MetricSubtype, double>& healthMetricCache)
+{
+    std::ifstream memInfo(procMeminfo);
+    if (!memInfo.is_open())
+    {
+        error("Unable to open {PATH} for reading Memory stats", "PATH",
+              procMeminfo);
+        return false;
+    }
+    std::string line;
+    std::unordered_map<MetricSubtype, double> memoryValues;
+
+    while (std::getline(memInfo, line))
+    {
+        std::string name;
+        double value;
+        std::istringstream iss(line);
+
+        if (!(iss >> name >> value))
+        {
+            continue;
+        }
+        if (name.starts_with(memoryAvailable))
+        {
+            memoryValues[MetricSubtype::memoryAvailable] = value;
+        }
+        else if (name.starts_with(memoryFree))
+        {
+            memoryValues[MetricSubtype::memoryFree] = value;
+        }
+        else if (name.starts_with(memoryBuffers) ||
+                 name.starts_with(memoryCached))
+        {
+            memoryValues[MetricSubtype::memoryBufferedAndCached] += value;
+        }
+        else if (name.starts_with(memoryTotal))
+        {
+            memoryValues[MetricSubtype::memoryTotal] = value;
+        }
+        else if (name.starts_with(memoryShared))
+        {
+            memoryValues[MetricSubtype::memoryShared] = value;
+        }
+    }
+
+    for (auto& metricConfig : healthMetricConfigs)
+    {
+        healthMetricCache[metricConfig.metricSubtype] =
+            (memoryValues.find(metricConfig.metricSubtype) != memoryValues.end()
+                 ? memoryValues[metricConfig.metricSubtype] * BytesMultiplier
+                 : std::numeric_limits<double>::quiet_NaN());
+        debug("Memory Metric {METRIC_SUBTYPE}: {METRIC_VALUE}",
+              "METRIC_SUBTYPE", std::to_underlying(metricConfig.metricSubtype),
+              "METRIC_VALUE", healthMetricCache[metricConfig.metricSubtype]);
+    }
+
+    return true;
+}
+
+static bool readStorageMetric(
+    const std::vector<HealthMetricConfig>& healthMetricConfigs,
+    std::unordered_map<MetricSubtype, double>& healthMetricCache)
+{
+    return false;
+}
+
+const std::map<MetricType,
+               std::function<bool(const std::vector<HealthMetricConfig>&,
+                                  std::unordered_map<MetricSubtype, double>&)>>
+    metricMap = {
+        {MetricType::CPU, readCPUMetric},
+        {MetricType::memory, readMemoryMetric},
+        {MetricType::storage, readStorageMetric},
+};
+
+void HealthMetricCollection::readHealthMetricCollection()
+{
+    // Read the health metrics from the system
+    auto metricHandler = metricMap.find(metricType);
+    if (metricHandler != metricMap.end())
+    {
+        if (!(*metricHandler).second(healthMetricConfigs, healthMetricCache))
+        {
+            error("Failed to read health metric {METRIC_TYPE}", "METRIC_TYPE",
+                  std::to_underlying(metricType));
+            return;
+        }
+    }
+    else
+    {
+        error("No read metric handler found for {METRIC_TYPE}", "METRIC_TYPE",
+              std::to_underlying(metricType));
+        return;
+    }
+
+    for (auto& metric : healthMetrics)
+    {
+        info("Update health metric {METRIC_SUBTYPE} with value {METRIC_VALUE}",
+             "METRIC_SUBTYPE", std::to_underlying(metric.first), "METRIC_VALUE",
+             healthMetricCache[metric.first]);
+        metric.second->updateHealthMetric(healthMetricCache[metric.first]);
+    }
+}
+
+void HealthMetricCollection::createHealthMetricCollection(
+    const std::vector<std::string>& bmcInventoryPaths)
+{
+    // Clear the cache for any existing values
+    healthMetricCache.clear();
+    healthMetrics.clear();
+
+    // Read the health metrics from the system
+    auto metricHandler = metricMap.find(metricType);
+    if (metricHandler != metricMap.end())
+    {
+        if (!(*metricHandler).second(healthMetricConfigs, healthMetricCache))
+        {
+            error("Failed to read health metric {METRIC_TYPE}", "METRIC_TYPE",
+                  std::to_underlying(metricType));
+            return;
+        }
+    }
+    else
+    {
+        error("No read metric handler found for {METRIC_TYPE}", "METRIC_TYPE",
+              std::to_underlying(metricType));
+        return;
+    }
+
+    for (auto& metricConfig : healthMetricConfigs)
+    {
+        healthMetrics[metricConfig.metricSubtype] =
+            std::make_unique<HealthMetric>(bus, metricType, metricConfig,
+                                           bmcInventoryPaths);
+    }
+
+    // TODO: If needed start the monitoring for this collection
+}
+
+} // namespace phosphor::health::metric::collection
---
+++ test/test_health_metric_collection.cpp
@@ -0,0 +1,90 @@
+#include "health_metric_collection.hpp"
+#include "health_utils.hpp"
+
+#include <sdbusplus/test/sdbus_mock.hpp>
+#include <xyz/openbmc_project/Metrics/Value/server.hpp>
+
+#include <gtest/gtest.h>
+
+using namespace phosphor::health::metric::config;
+using namespace phosphor::health::metric;
+using namespace phosphor::health::metric::collection;
+using namespace phosphor::health::utils;
+using PathInterface =
+    sdbusplus::common::xyz::openbmc_project::metrics::Value::namespace_path;
+using ::testing::Invoke;
+using ::testing::IsNull;
+using ::testing::NotNull;
+using ::testing::StrEq;
+
+constexpr auto BMCInventoryItem = "xyz.openbmc_project.Inventory.Item.Bmc";
+
+class HealthMetricCollectionTest : public ::testing::Test
+{
+  public:
+    sdbusplus::SdBusMock sdbusMock;
+    sdbusplus::bus::bus bus = sdbusplus::get_mocked_new(&sdbusMock);
+
+    static constexpr auto busName = "xyz.openbmc_project.test.HealthMon";
+    static constexpr auto objPath = "/xyz/openbmc_project/sdbusplus/test";
+    std::map<MetricType, std::vector<HealthMetricConfig>> healthMetricConfigs;
+
+    void SetUp() override
+    {
+        healthMetricConfigs = getHealthMetricConfigs();
+        EXPECT_THAT(healthMetricConfigs.size(), testing::Ge(1));
+        // Update the health metric window size to 1 for test purposes
+        for (auto& [type, configs] : healthMetricConfigs)
+        {
+            for (auto& config : configs)
+            {
+                config.windowSize = 1;
+            }
+        }
+    }
+};
+
+TEST_F(HealthMetricCollectionTest, TestHealthMetricCollectionCreation)
+{
+    sdbusplus::server::manager_t objManager(bus, objPath);
+    bus.request_name(busName);
+
+    const std::string valueInterface = "xyz.openbmc_project.Metrics.Value";
+    const std::string thresholdInterface =
+        "xyz.openbmc_project.Common.Threshold";
+    const std::set<std::string> valueProperties = {"Value", "MaxValue",
+                                                   "MinValue", "Unit"};
+    const std::set<std::string> thresholdProperties = {"Value", "Asserted"};
+
+    std::vector<std::string> bmcInventoryPaths =
+        findPathsWithType(bus, BMCInventoryItem);
+    std::map<MetricType, std::unique_ptr<HealthMetricCollection>>
+        healthMetricCollections;
+
+    EXPECT_CALL(sdbusMock,
+                sd_bus_emit_properties_changed_strv(
+                    IsNull(), NotNull(), StrEq(valueInterface), NotNull()))
+        .WillRepeatedly(Invoke(
+            [&]([[maybe_unused]] sd_bus* bus, [[maybe_unused]] const char* path,
+                [[maybe_unused]] const char* interface, const char** names) {
+        EXPECT_THAT(valueProperties, testing::Contains(names[0]));
+        return 0;
+    }));
+    EXPECT_CALL(sdbusMock,
+                sd_bus_emit_properties_changed_strv(
+                    IsNull(), NotNull(), StrEq(thresholdInterface), NotNull()))
+        .WillRepeatedly(Invoke(
+            [&]([[maybe_unused]] sd_bus* bus, [[maybe_unused]] const char* path,
+                [[maybe_unused]] const char* interface, const char** names) {
+        EXPECT_THAT(thresholdProperties, testing::Contains(names[0]));
+        return 0;
+    }));
+
+    for (const auto& [type, configs] : healthMetricConfigs)
+    {
+        healthMetricCollections[type] =
+            std::make_unique<HealthMetricCollection>(bus, type, configs,
+                                                     bmcInventoryPaths);
+        healthMetricCollections[type]->readHealthMetricCollection();
+    }
+}
