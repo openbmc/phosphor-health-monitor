e9896140fab0833979ea526e206c04521628ba86
Jagpal Singh Gill <paligill@gmail.com>
2023-12-10 15:23:19 -0800

    add health_metric interface and implementation
    Add the interface and implementation for the health_metric to be used
    in the rewrite for phosphor-health-monitor.
    This change is in relation to following design and D-Bus interface
    update -
    https://gerrit.openbmc.org/c/openbmc/docs/+/64917
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/64914
    
    gtest added for UT.
    
    Change-Id: Iffcc52f9dff712890377b1222fd7e7d5d6661eaf
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ health_utils.cpp
@@ -0,0 +1,65 @@
+#include "health_metric.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/sdbus.hpp>
+
+PHOSPHOR_LOG2_USING;
+
+namespace phosphor
+{
+namespace health
+{
+namespace utils
+{
+
+const char* inventoryPath = "/xyz/openbmc_project/inventory";
+
+void startUnit(sdbusplus::bus_t& bus, const std::string& sysdUnit)
+{
+    if (sysdUnit.empty())
+    {
+        return;
+    }
+    sdbusplus::message_t msg = bus.new_method_call(
+        "org.freedesktop.systemd1", "/org/freedesktop/systemd1",
+        "org.freedesktop.systemd1.Manager", "StartUnit");
+    msg.append(sysdUnit, "replace");
+    bus.call_noreply(msg);
+}
+
+std::vector<std::string> findPathsWithType(sdbusplus::bus_t& bus,
+                                           const std::string& iface)
+{
+    std::vector<std::string> ret;
+
+    sdbusplus::message_t msg = bus.new_method_call(
+        "xyz.openbmc_project.ObjectMapper",
+        "/xyz/openbmc_project/object_mapper",
+        "xyz.openbmc_project.ObjectMapper", "GetSubTreePaths");
+
+    msg.append("/", 0, std::vector<std::string>{iface});
+
+    try
+    {
+        bus.call(msg, 0).read(ret);
+        if (!ret.empty())
+        {
+            debug("{IFACE} found", "IFACE", iface);
+        }
+        else
+        {
+            debug("{IFACE} not found", "IFACE", iface);
+        }
+    }
+    catch (std::exception& e)
+    {
+        error("Exception occurred while calling {PATH}: {ERROR}", "PATH",
+              inventoryPath, "ERROR", e);
+    }
+    return ret;
+}
+
+} // namespace utils
+} // namespace health
+} // namespace phosphor
