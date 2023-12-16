32dacd6f98d25dd396df4e628b938f2cefbedd51
Jagpal Singh Gill <paligill@gmail.com>
2023-12-15 17:01:03 -0800

    enable health_monitor new implementation
    Add health_monitor interface and implementation. Enable the health_monitor
    from existing healthMonitor flow.
    This change is in relation to following design and D-Bus interface update -
    https://gerrit.openbmc.org/c/openbmc/docs/+/64917
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/64914
    
    Change-Id: I1e9fac226b438f0556cb20321f1f2b6d51af7dbc
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ health_monitor.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "health_metric_collection.hpp"
+
+#include <xyz/openbmc_project/Association/Definitions/server.hpp>
+#include <xyz/openbmc_project/Common/Threshold/server.hpp>
+#include <xyz/openbmc_project/Inventory/Item/Bmc/server.hpp>
+#include <xyz/openbmc_project/Metric/Value/server.hpp>
+
+#include <map>
+
+namespace phosphor
+{
+namespace health
+{
+namespace monitor
+{
+using namespace phosphor::health::metric::config;
+using namespace phosphor::health::metric::collection;
+class HealthMonitor
+{
+  public:
+    HealthMonitor() = delete;
+    virtual ~HealthMonitor() = default;
+
+    HealthMonitor(sdbusplus::bus_t& bus) :
+        bus(bus), healthMetricConfigs(getHealthMetricConfigs())
+    {
+        createHealthMonitor();
+    }
+
+    bool runHealthMonitor();
+
+  private:
+    sdbusplus::bus_t& bus;
+    void createHealthMonitor();
+    std::map<MetricType, std::vector<HealthMetricConfig>> healthMetricConfigs;
+    std::map<MetricType, std::unique_ptr<HealthMetricCollection>>
+        healthMetricCollections;
+};
+} // namespace monitor
+} // namespace health
+} // namespace phosphor
