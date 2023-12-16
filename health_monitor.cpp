7c606404b8107434945dc8f81e0ca424b5d8661a
Jagpal Singh Gill <paligill@gmail.com>
2023-12-15 17:01:03 -0800

    enable health_monitor new implementation
    Add health_monitor interface and implementation. Enable the
    health_monitor from existing healthMonitor flow.
    This change is in relation to following design and D-Bus interface
    update -
    https://gerrit.openbmc.org/c/openbmc/docs/+/64917
    https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/64914
    
    Tested:
    New interfaces being shown after the change.
    `- /xyz
      `- /xyz/openbmc_project
        |- /xyz/openbmc_project/metric
        | `- /xyz/openbmc_project/metric/bmc
        |   |- /xyz/openbmc_project/metric/bmc/cpu
        |   | |- /xyz/openbmc_project/metric/bmc/cpu/kernel
        |   | |- /xyz/openbmc_project/metric/bmc/cpu/total
        |   | `- /xyz/openbmc_project/metric/bmc/cpu/user
        |   |- /xyz/openbmc_project/metric/bmc/memory
        |   | |- /xyz/openbmc_project/metric/bmc/memory/available
        |   | `- /xyz/openbmc_project/metric/bmc/memory/total
        |   `- /xyz/openbmc_project/metric/bmc/storage
        |     `- /xyz/openbmc_project/metric/bmc/storage/rw
    
    Change-Id: I1e9fac226b438f0556cb20321f1f2b6d51af7dbc
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ health_monitor.cpp
@@ -0,0 +1,68 @@
+#include "health_monitor.hpp"
+
+#include <phosphor-logging/lg2.hpp>
+#include <sdbusplus/async.hpp>
+
+PHOSPHOR_LOG2_USING;
+
+namespace phosphor::health::monitor
+{
+
+using namespace phosphor::health::utils;
+
+void HealthMonitor::create()
+{
+    info("Creating Health Monitor with config size {CSIZE}", "CSIZE",
+         configs.size());
+    constexpr auto BMCInventoryItem = "xyz.openbmc_project.Inventory.Item.Bmc";
+    auto bmcPaths = findPaths(bus, BMCInventoryItem);
+
+    for (auto& [type, collectionConfig] : configs)
+    {
+        info("Creating Health Metric Collection for {MTYPE}", "MTYPE",
+             std::to_underlying(type));
+        collections[type] =
+            std::make_unique<CollectionIntf::HealthMetricCollection>(
+                bus, type, collectionConfig, bmcPaths);
+    }
+}
+
+auto HealthMonitor::run() -> bool
+{
+    info("Running Health Monitor");
+    for (auto& [type, collection] : collections)
+    {
+        info("Reading Health Metric Collection for {MTYPE}", "MTYPE",
+             std::to_underlying(type));
+        collection->read();
+    }
+
+    return true;
+}
+
+int startMonitor()
+{
+    constexpr auto path = MetricIntf::ValueIntf::Value::namespace_path::value;
+    sdbusplus::async::context ctx;
+    sdbusplus::server::manager_t manager{ctx, path};
+    constexpr auto healthMonitorServiceName =
+        "xyz.openbmc_project.bmc_health_monitor";
+    ctx.get_bus().request_name(healthMonitorServiceName);
+
+    HealthMonitor healthMonitor{ctx.get_bus()};
+
+    ctx.spawn([](sdbusplus::async::context& ctx,
+                 HealthMonitor& healthMonitor) -> sdbusplus::async::task<> {
+        while (true)
+        {
+            healthMonitor.run();
+            co_await sdbusplus::async::sleep_for(
+                ctx, std::chrono::milliseconds(5 * 1000));
+        }
+    }(ctx, healthMonitor));
+
+    ctx.run();
+    return 0;
+}
+
+} // namespace phosphor::health::monitor
