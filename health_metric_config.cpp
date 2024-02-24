2c2b72f324cf6f7691c0d3261af0d3d46fbf74e2
Patrick Williams <patrick@stwcx.xyz>
2024-02-23 19:07:32 -0600

    metric-config: remove thresholds on userspace and kernel CPU
    We do not need a default threshold on the userspace and kernel CPU
    metrics because we already have a threshold on the overall CPU usage.
    Sustained CPU usage is likely what someone wants to identify, no matter
    which side of the accounting it is attributed to.  Delete the extra
    thresholds.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I56352169b3fc50f11410f054053c39c40a79b82e

--- health_metric_config.cpp
+++ health_metric_config.cpp
@@ -212,34 +212,10 @@ json defaultHealthMetricConfig = R"({
     "CPU_User": {
         "Frequency": 1,
         "Window_size": 120,
-        "Threshold": {
-            "Critical_Upper": {
-                "Value": 90.0,
-                "Log": true,
-                "Target": ""
-            },
-            "Warning_Upper": {
-                "Value": 80.0,
-                "Log": false,
-                "Target": ""
-            }
-        }
     },
     "CPU_Kernel": {
         "Frequency": 1,
         "Window_size": 120,
-        "Threshold": {
-            "Critical_Upper": {
-                "Value": 90.0,
-                "Log": true,
-                "Target": ""
-            },
-            "Warning_Upper": {
-                "Value": 80.0,
-                "Log": false,
-                "Target": ""
-            }
-        }
     },
     "Memory": {
         "Frequency": 1,
